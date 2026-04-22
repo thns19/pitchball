be343c487ccc21246dfdf2faa910fae050985093
thns19 <thanos19chatz@gmail.com>
2026-04-22 19:09:15 +0200

    Add files via upload

--- psl5/matches.js
+++ psl5/matches.js
@@ -164,6 +164,99 @@ const matchData = [
     ]
   },
   {
+    id:"MD2_YY_BasementBoys",
+    home: "Y&Y", away:"Basement Boys", hs: 5, as: 1,
+    date: "16/02/2026, 12:22PM", mvp: "Giannis Akridas",
+    events: [
+      {type: "og", team:"Y&Y", player:"Jordan Aslanis", hs:5, as:1},
+      {type: "og", team:"Basement Boys", player:"Giorgos Filippou", hs:4, as:1},
+      {type: "og", team:"Y&Y", player:"Jordan Aslanis", hs:4, as:0},
+      {type: "og", team:"Y&Y", player:"Jordan Aslanis", hs:3, as:0},
+      {type:"goal", team:"Y&Y", player:"Giorgos Filippou", hs:2, as:0},
+      {type: "goal", team:"Y&Y", player:"Giannis Akridas", hs:1, as:0},
+    ]
+  },
+  {
+    id:"MD2_VolosDrummers_Babiniotes",
+    home: "Volos Drummers", away:"Babiniotes", hs:5, as:3,
+    date: "18/02/2026, 12:24PM", mvp: "Giorgos Zacharopoulos",
+    events: [
+      {type: "og", team:"Volos Drummers", player:"Stavros Stavropoulos", hs:5, as:3},
+      {type: "og", team:"Volos Drummers", player:"Stavros Stavropoulos", hs:4, as:3},
+      {type: "goal", team:"Volos Drummers", player:"Vasilis Tziovanis", hs:3, as:3},
+      {type: "og", team:"Volos Drummers", player:"Johan Pilichos", hs:2, as:3},
+      {type: "og", team:"Volos Drummers", player:"Stavros Stavropoulos", hs:1, as:3},
+      {type: "og", team:"Babiniotes", player:"Giorgos Zacharopoulos", hs:0, as:3},
+      {type: "og", team:"Babiniotes", player:"Giorgos Zacharopoulos", hs:0, as:2},
+      {type: "og", team:"Babiniotes", player:"Vasilis Tziovanis", hs:0, as:1}
+    ]
+  },
+  {
+    id: "MD2_Warriors_R1",
+    home: "Warriors", away: "R1", hs:1,as:5,
+    date:"17/02/2026, 09:51AM",mvp:"Nikolas Moschonas",
+    events: [
+      {type:"goal",team:"R1",player:"Nikolas Moschonas",hs:1,as:5},
+      {type:"goal",team:"R1",player:"Nikolas Moschonas",hs:1,as:4},
+      {type:"goal",team:"R1",player:"Nikolas Moschonas",hs:1,as:3},
+      {type:"goal",team:"R1",player:"Nikolas Moschonas",hs:1,as:2},
+      {type:"og",team:"Warriors",player:"Michalis Lerogiannis",hs:1,as:1},
+      {type:"goal",team:"R1",player:"Nikolas Moschonas",hs:0,as:1},
+    ]
+  },
+  {
+    id:"MD3_MidiKidz_YY",
+    home:"Midi Kidz",away:"Y&Y",hs:3,as:5,
+    date:"18/03/2026, 12:20PM",mvp:"Giannis Akridas",
+    events:[
+      {type:"goal",team:"Y&Y",player:"Giannis Akridas",hs:3,as:5},
+      {type:"og",team:"Midi Kidz",player:"Giorgos Filippou",hs:3,as:4},
+      {type:"og",team:"Y&Y",player:"Tzannis Mermigas",hs:2,as:4},
+      {type:"goal",team:"Y&Y",player:"Giorgos Filippou",hs:2,as:3},
+      {type:"og",team:"Y&Y",player:"Stefanos Mavrogiannis",hs:2,as:2},
+      {type:"og",team:"Midi Kidz",player:"Giorgos Filippou",hs:2,as:1},
+      {type:"goal",team:"Y&Y",player:"Giannis Akridas",hs:1,as:1},
+      {type:"og",team:"Midi Kidz",player:"Giorgos Filippou",hs:1,as:0},
+    ]
+  },
+  {
+    id:"MD3_BADiles_VolosDrummers",
+    home:"BADiles",away:"Volos Drummers",hs:5,as:0,
+    date:"19/03/2026, 11:30AM",mvp:"Dimitris Gavalas",
+    events:[
+      {type:"goal",team:"BADiles",player:"Dimitris Gavalas",hs:5,as:0},
+      {type:"goal",team:"BADiles",player:"Dimitris Gavalas",hs:4,as:0},
+      {type:"og",team:"BADiles",player:"Giorgos Zacharopoulos",hs:3,as:0},
+      {type:"goal",team:"BADiles",player:"Thanos Chatziiordanou",hs:2,as:0},
+      {type:"og",team:"BADiles",player:"Vasilis Tziovanis",hs:1,as:0}
+    ]
+  },
+  {
+    id:"MD3_Babiniotes_Warriors",
+    home:"Babiniotes",away:"Warriors",hs:3,as:5,
+    date:"13/03/2026, 10:44AM",mvp:"Sokratis Gkiolias",
+    events:[
+      {type:"og",team:"Warriors",player:"Stavros Stavropoulos",hs:3,as:5},
+      {type:"og",team:"Babiniotes",player:"Panos Chardas",hs:3,as:4},
+      {type:"og",team:"Babiniotes",player:"Panos Chardas",hs:2,as:4},
+      {type:"og",team:"Warriors",player:"Stavros Stavropoulos",hs:1,as:4},
+      {type:"og",team:"Warriors",player:"Johan Pilichos",hs:1,as:3},
+      {type:"og",team:"Babiniotes",player:"Panos Chardas",hs:1,as:2},
+      {type:"og",team:"Warriors",player:"Stavros Stavropoulos",hs:0,as:2},
+      {type:"og",team:"Warriors",player:"Stavros Stavropoulos",hs:0,as:1},
+    ]
+  },
+  {
+    id:"MD3_SpasmenaMila_R1",
+    home:"Spasmena Mila",away:"R1",hs:1,as:5,
+    date: null, mvp: null,
+    events: [
+      {type:"og",team:"R1",player:"Spasmena Mila forfeit",hs:1,as:5},
+      {type:"og",team:"R1",player:"Aris Tsertos",hs:1,as:1},
+      {type:"og",team:"Spasmena Mila",player:"Michalis Lerogiannis",hs:1,as:0},
+    ]
+  },
+  {
     id:"MD6_Babiniotes_YY",
     home:"Babiniotes",away:"Y&Y",hs:2,as:5,
     date: "22/04/2026, ??:??AM", mvp:"Giorgos Filippou",
